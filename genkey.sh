eb73974fe6c83211ab1dfd36125924b89e53b8d2
root <root@debug010000002015.local.alipay.net>
2020-04-10 15:20:25 +0800

    add note

--- genkey.sh
+++ genkey.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# note: The Common Name (AKA CN) represents the hostname protected by the SSL certificate.
 openssl genrsa -out privkey.pem 2048
 openssl req -new -x509 -key privkey.pem -out cert.pem -days 365

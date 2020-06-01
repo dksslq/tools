0a667a74b97ffd3de374d051a3d3520bd320d385
root <root@debug010000002015.local.alipay.net>
2020-06-01 15:52:55 +0800

    fixup

--- genkey.sh
+++ genkey.sh
@@ -2,4 +2,4 @@
 
 # The Common Name (AKA CN) represents the server name protected by the SSL certificate. The certificate is valid only if the request hostname matches the certificate common name.
 openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key privkey.pem -out cert.pem -days 365
+openssl req -new -x509 -key key.pem -out cert.pem -days 365

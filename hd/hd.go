03c63a818abee1275a14954fd852abf14e827cce
root <root@debug010000002015.local.alipay.net>
2020-07-21 11:55:26 +0800

    fixup

--- hd/hd.go
+++ hd/hd.go
@@ -17,17 +17,7 @@ func main() {
 	}
 	defer file.Close()
 
-	fileinfo, err := file.Stat()
-	if err != nil {
-		log.Fatal("[stat file] ", err)
-	}
-	filesize := fileinfo.Size()
-
 	buf := make([]byte, 40960, 40960)
-	fmt.Printf("%s%d%s\n", "unsigned char buf[", filesize, "] = {")
-	fmt.Printf("\t")
-
-	var count int64 = 1
 
 	for {
 		n, err := file.Read(buf)
@@ -42,20 +32,8 @@ func main() {
 
 		i := 0
 		for i < n {
-			fmt.Printf("0x%02X", buf[i])
-
-			if count != filesize {
-				/*逗号分隔*/
-				fmt.Printf(", ")
-				/*每十六字节并且不在文件尾时 换行*/
-				if count%16 == 0 {
-					fmt.Printf("\n\t")
-				}
-			}
+			fmt.Printf("%02X", buf[i])
 			i++
-			count++
 		}
 	}
-
-	fmt.Println("\n};")
 }
